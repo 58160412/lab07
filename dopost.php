f7a6d5cefc0c8089a854b80407ffd30eca9e05ae
58160412 <58160412@go.buu.ac.th>
2017-03-22 17:40:05 +0700

    w

---
+++ dopost.php
@@ -0,0 +1,20 @@
+
+<h3>View posted data:</h3><pre>Array
+(
+)
+</pre><hr><h3>View individual data:</h3><br /><br /><br /><html>
+<head>
+<meta charset="utf-8">
+<title>ผลการลงทะเบียน</title>
+
+</head>
+<body>
+<br>
+<button class="button" style="vertical-align:middle" name="info" onclick="linkto();"><span>Info</span></button>
+<script>
+    function linkto() {
+        window.location='http://angsila.cs.buu.ac.th/~58160412/887371/lab07/show_register.php';
+    }
+</script>
+</body>
+</html>
