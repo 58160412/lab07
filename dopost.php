4238ba7908d799cf4203bf29f9de5b4ce79a56cd
58160412 <58160412@go.buu.ac.th>
2017-03-23 15:52:29 +0700

    kk

--- dopost.php
+++ dopost.php
@@ -6,11 +6,11 @@ print_r($_POST);
 echo "</pre>";
 echo "<hr>";
 echo "<h3>View individual data:</h3>";
-echo $_POST['name'] . "<br />";
+echo $_POST['fullname'] . "<br />";
 echo $_POST['email'] . "<br />";
 echo $_POST['address'] . "<br />";
 
-$name = $_POST['name'];
+$fulllname = $_POST['fullname'];
 $email = $_POST['email'];
 $sex = $_POST['sex'];
 $intr1 = $_POST['intr1'];
@@ -19,7 +19,7 @@ $address = $_POST['address'];
 $province = $_POST['province'];
 
 if (isset($_POST['submit'])) {
-    $sql = "INSERT INTO register (fullName,email,sex,intr1,intr2,address,provinces) VALUES ('$name','$email','$sex','$intr1','$intr2','$address',$province)";
+    $sql = "INSERT INTO register (fullname,email,sex,intr1,intr2,address,province) VALUES ('$fullname','$email','$sex','$intr1','$intr2','$address',$province)";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
@@ -34,47 +34,7 @@ $conn->close();
 <head>
 <meta charset="utf-8">
 <title>ผลการลงทะเบียน</title>
-<style>
-.button {
-  display: inline-block;
-  border-radius: 4px;
-  background-color: #f4511e;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 28px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
 
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-
-.button:hover span {
-  padding-right: 25px;
-}
-
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-</style>
 </head>
 <body>
 <br>
