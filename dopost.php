349d6dfafbe7f5db44c79037fdac80fa5ca5a166
58160412 <58160412@go.buu.ac.th>
2017-03-23 17:17:23 +0700

    bb

--- dopost.php
+++ dopost.php
@@ -6,7 +6,7 @@ print_r($_POST);
 echo "</pre>";
 echo "<hr>";
 echo "<h3>View individual data:</h3>";
-echo $_POST['fullname'] . "<br />";
+echo $_POST['name'] . "<br />";
 echo $_POST['email'] . "<br />";
 echo $_POST['address'] . "<br />";
 
@@ -16,10 +16,10 @@ $sex = $_POST['sex'];
 $intr1 = $_POST['intr1'];
 $intr2 = $_POST['intr2'];
 $address = $_POST['address'];
-$province = $_POST['provinces'];
+$province = $_POST['province'];
 
 if (isset($_POST['submit'])) {
-    $sql = "INSERT INTO register (fullname,email,sex,intr1,intr2,address,provinces) VALUES ('$fullname','$email','$sex','$intr1','$intr2','$address',$provinces)";
+    $sql = "INSERT INTO register (fullname,email,sex,intr1,intr2,address,provinces) VALUES ('$name','$email','$sex','$intr1','$intr2','$address',$province)";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
